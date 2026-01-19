d3084b9ee3d1af43ab997b93257efacd8c04bb98
Loïc Chagnoleau <furlings@pm.me>
2026-01-19 15:25:13 +0100

    nix-darwin

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # My dotfiles using [chezmoi](https://www.chezmoi.io/)
 
 ```bash
+# Homebrew is needed for git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install git
+brew install git
+# Rosetta
+softwareupdate --install-rosetta
+# Nix
+curl -sSf -L https://install.lix.systems/lix | sh -s -- install
+# chezmoi
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply lchagnoleau
+# In a new shell:
+sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake ~/.config/nix#BigMac
+```
+
+## Ugrade Nix pacakages
+
+```bash
+#update packages
+nix flake update
+sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake ~/.config/nix#BigMac
 ```
---
+++ dot_config/nix/flake.nix
@@ -0,0 +1,157 @@
+{
+  description = "BigMac nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+  let
+    configuration = { pkgs, ... }: {
+      system.primaryUser = "lchagnoleau";
+      nixpkgs.config.allowUnfree = true;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.neovim
+          pkgs.tmux
+          pkgs.git
+          pkgs.delta
+          pkgs.eza
+          pkgs.zoxide
+          pkgs.jujutsu
+          pkgs.lazygit
+          pkgs.chezmoi
+          pkgs.firefox
+          pkgs.ghostty-bin
+          pkgs.ripgrep
+          pkgs.fzf
+          pkgs.fd
+        ];
+
+      homebrew = {
+        enable = true;
+          casks = [
+            "the-unarchiver"
+            "iina"
+            "rectangle"
+            "hyperkey"
+            "maccy"
+            # "wireguard"
+            "parallels"
+            "microsoft-office"
+            "microsoft-teams"
+          ];
+          brews = [
+            "mole"
+            "mas"
+            "mise"
+            "age"
+          ];
+          masApps = {
+            # "Xcode" = 497799835;
+            "WireGuard" = 1451685025;
+          };
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+        };
+
+      fonts.packages =
+        with pkgs; [
+          nerd-fonts.hack
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # MacOs settings
+      system.defaults = {
+        dock.show-recents = false;
+        dock.autohide = true;
+        dock.autohide-delay = 0.0;
+        dock.autohide-time-modifier = 0.4;
+        dock.tilesize = 48;
+        dock.largesize = 64;
+        dock.mru-spaces = false;
+        dock.minimize-to-application = true;
+        dock.magnification = true;
+        dock.show-process-indicators = false;
+        dock.scroll-to-open = true;
+        dock.static-only = true;
+        dock.persistent-apps = [
+          "${pkgs.ghostty-bin}/Applications/Ghostty.app"
+          "${pkgs.firefox}/Applications/Firefox.app"
+        ];
+        controlcenter.FocusModes = false;
+        finder.FXPreferredViewStyle = "clmv";
+        finder.AppleShowAllExtensions = true;
+        finder.ShowPathbar = true;
+        finder.ShowStatusBar = true;
+        finder.AppleShowAllFiles = false;
+        finder.NewWindowTarget = "Home";
+        screencapture.location = "~/Screenshots";
+        loginwindow.GuestEnabled = false;
+        trackpad.Clicking = true;
+        screensaver.askForPassword = true;
+        screensaver.askForPasswordDelay = 0;
+        CustomUserPreferences.NSGlobalDomain."com.apple.mouse.linear" = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+        NSGlobalDomain.InitialKeyRepeat = 15;
+        NSGlobalDomain."com.apple.swipescrolldirection" = false;
+        NSGlobalDomain."com.apple.sound.beep.feedback" = 0;
+        NSGlobalDomain."com.apple.trackpad.scaling" = 2.5;
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."BigMac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "lchagnoleau";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+  };
+}
